6e91dfa32814e85eb6e8f9fd4728e35b0b5c169b
F. S. Farimani <foad.farimani@nts-group.nl>
2024-12-06 11:45:34 +0100

    Add project files.

---
+++ src/CustomBuildTasks/LineCounter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace CustomBuildTasks
+{
+    public class LineCounter : Task
+    {
+        [Required]
+        public ITaskItem[] SourceFiles { get; set; }
+
+        [Output]
+        public int TotalLines { get; set; }
+
+        public override bool Execute()
+        {
+            try
+            {
+                TotalLines = SourceFiles
+                    .Select(file => File.ReadAllLines(file.ItemSpec).Length)
+                    .Sum();
+
+                Log.LogMessage(MessageImportance.High,
+                    $"Total lines of code: {TotalLines}");
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.LogError($"Error counting lines: {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
